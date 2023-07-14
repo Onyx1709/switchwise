a56a69f8eb87c2be5ad8b4964f224588d4fcf05f
kingkite <kitewebdevelopers@gmail.com>
2023-07-14 12:43:53 +0100

    Next Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="text-center text-lg text-red-600">Hello World</h1>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	mode: 'jit',
+	darkMode: 'media',
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					100: '#6565cd',
+					200: '#5252c7',
+					300: '#3e3ec1',
+					400: '#252574',
+					500: '#1f1f60',
+					600: '#19194d',
+					700: '#13133a',
+					800: '#0c0c27',
+					900: '#060613',
+				},
+				secondary: {
+					100: '#ffa366',
+					200: '#ff944d',
+					300: '#ff8533',
+					400: '#ff751a',
+					500: '#ff6600',
+					600: '#e65c00',
+					700: '#cc5200',
+					800: '#b34700',
+					900: '#993d00',
+				},
+			},
+			screens: {
+				xs: '400px',
+				sm: '580px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px',
+			},
+		},
+	},
+	variants: {
+		animation: ['responsive', 'motion-safe', 'motion-reduce'],
+		backgroundColor: ['responsive', 'odd', 'hover', 'focus', 'even', 'active'],
+		borderWidth: ['responsive', 'hover', 'focus'],
+		fontSize: ['responsive', 'hover', 'focus'],
+		extend: {},
+		position: ['responsive'],
+	},
+	plugins: [],
+};
