cca71d6abce275e665b723200b96dfacdce89490
kingkite <kitewebdevelopers@gmail.com>
2023-07-14 17:50:43 +0100

    Initiation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	corePlugins: {
+		preflight: false,
+	},
 	mode: 'jit',
 	darkMode: 'media',
 	theme: {
