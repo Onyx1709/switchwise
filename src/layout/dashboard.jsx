f03e33e65b0caa31cd59075b0f2ee96b6d6df9e7
kingkite <kitewebdevelopers@gmail.com>
2023-07-15 03:53:39 +0100

    Got the mobile sidebar looking good

--- src/layout/dashboard.jsx
+++ src/layout/dashboard.jsx
@@ -8,28 +8,34 @@ function Layout() {
 	const menu = useOutsideClick();
 
 	return (
-		<div className="w-full">
-			<div className="bg-gray-100 flex items-center justify-between px-4 py-2 md:px-6 md:py-3 lg:hidden">
+		<div className="overflow-x-hidden w-full">
+			<div className="bg-gray-100 flex items-center justify-between p-4 md:px-6 md:py-3 lg:hidden">
 				{/* Logo */}
-				<div className="h-[21px] mx-auto w-[120px] md:h-[32px] md:w-[181px]">
-					<img
-						className="h-full w-full md:hidden"
-						src="/images/mobile-login-switchwise.png"
-						alt="SwitchWise"
-					/>
-					<img
-						className="hidden h-full w-full md:block"
-						src="/images/desktop-login-switchwise.png"
-						alt="SwitchWise"
-					/>
+				<div>
+					<div className="h-[21px] mx-auto w-[120px] md:h-[32px] md:w-[181px]">
+						<img
+							className="h-full w-full md:hidden"
+							src="/images/mobile-login-switchwise.png"
+							alt="SwitchWise"
+						/>
+						<img
+							className="hidden h-full w-full md:block"
+							src="/images/desktop-login-switchwise.png"
+							alt="SwitchWise"
+						/>
+					</div>
 				</div>
 				{/* Logo */}
 
-				<span>
+				<span
+					className="cursor-pointer text-color-primary text-lg"
+					ref={menu.buttonRef}
+					onClick={() => menu.setVisible(true)}
+				>
 					<BarsOutlined />
 				</span>
 			</div>
-			<div className="flex relative">
+			<div className="flex">
 				<Sidebar
 					setVisible={menu.setVisible}
 					visible={menu.visible}
--- src/layout/sidebar.jsx
+++ src/layout/sidebar.jsx
@@ -1,23 +1,41 @@
+import {
+	LeftSquareOutlined,
+	CloseOutlined,
+	HomeOutlined,
+	SettingOutlined,
+	UserOutlined,
+} from '@ant-design/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import routes from '../config/routes';
 
 const sidebarStyle =
-	'absolute bg-primary-500 duration-1000 h-full overflow-y-auto transform top-16 w-3/4 z-50 sm:top-14 md:px-2 md:w-1/3 lg:fixed lg:px-0 lg:py-6 lg:top-0 lg:translate-x-0 lg:w-1/6 xl:py-7';
+	'bg-white duration-1000 h-full ml-auto overflow-y-auto shadow-lg transform w-3/5 md:px-2 md:w-1/3 lg:px-0 lg:py-6 lg:translate-x-0 lg:w-full xl:py-7';
 
 // eslint-disable-next-line react/prop-types
 const Sidebar = ({ setVisible, visible }, ref) => {
 	const links = React.useMemo(
 		() => [
 			{
-				icon: () => <></>,
-				title: 'dashboard',
+				icon: LeftSquareOutlined,
+				title: 'Home',
 				href: routes.DASHBOARD_PAGE,
 			},
 			{
-				icon: () => <></>,
-				title: 'file manager',
-				href: routes.ROOMS_PAGE,
+				icon: HomeOutlined,
+				title: 'Rooms',
+				links: [
+					{
+						title: 'ROOM 1',
+						href: routes.SETTINGS_PAGE,
+					},
+				],
+			},
+			{
+				icon: SettingOutlined,
+				title: 'Settings',
+				href: routes.SETTINGS_PAGE,
 			},
 		],
 		[]
@@ -25,19 +43,47 @@ const Sidebar = ({ setVisible, visible }, ref) => {
 
 	return (
 		<nav
-			ref={ref}
 			className={`${
-				visible ? 'translate-x-0' : '-translate-x-full'
-			} ${sidebarStyle}`}
+				visible ? 'opacity-100 z-100' : 'opacity-0 z-[-100]'
+			} duration-500 fixed h-full left-0 top-0 overflow-x-hidden w-full lg:bg-gray-100 lg:opacity-100 lg:shadow-lg lg:w-1/6 lg:z-0`}
+			ref={ref}
+			style={{
+				backgroundColor: 'rgba(0, 0, 0, 0.2)',
+			}}
 		>
-			<div className="mt-3">
-				{links.map(({ href, title, icon: Icon }, index) => {
-					return (
-						<a key={index} href={href} onClick={() => setVisible(false)}>
-							<Icon /> {title}
-						</a>
-					);
-				})}
+			<div
+				className={`${
+					visible ? 'translate-x-0' : 'translate-x-full'
+				} ${sidebarStyle}`}
+			>
+				<div className="flex items-center justify-between px-4 py-5">
+					<span
+						className="cursor-pointer text-color-primary text-lg"
+						onClick={() => setVisible(false)}
+					>
+						<CloseOutlined />
+					</span>
+					<span className="cursor-pointer text-color-primary text-lg">
+						<UserOutlined />
+					</span>
+				</div>
+				<div className="mt-3">
+					{links.map(({ href, title, icon: Icon }, index) => {
+						return (
+							<Link
+								className="cursor-pointer flex font-semibold items-center no-underline p-4 text-secondary-500 text-sm hover:bg-primary-500 hover:text-gray-100"
+								key={index}
+								href={href || '#'}
+								onClick={visible ? () => setVisible(false) : undefined}
+							>
+								<span className="mr-2">
+									<Icon />
+								</span>
+								<span className="">{title}</span>
+							</Link>
+						);
+					})}
+				</div>
 			</div>
 		</nav>
 	);
