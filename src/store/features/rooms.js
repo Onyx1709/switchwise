246630eea5917a1b1926c3c505167ee2b1611a28
kingkite <kitewebdevelopers@gmail.com>
2023-07-16 04:29:13 +0100

    Added action to change a device state for a specific room

--- src/store/features/rooms.js
+++ src/store/features/rooms.js
@@ -6,48 +6,120 @@ const initialState = {
 			id: 'living-room',
 			image: '/images/living-room.png',
 			name: 'Living Room',
-			devices: [],
+			devices: [
+				{
+					id: 1,
+					icon: 'bulb',
+					name: 'lights',
+					state: 'on',
+				},
+				{
+					id: 2,
+					icon: 'fan',
+					name: 'fan',
+					state: 'off',
+				},
+				{
+					id: 3,
+					icon: 'socket',
+					name: 'socket',
+					state: 'off',
+				},
+			],
 		},
 		{
 			id: 'bedroom-1',
 			image: '/images/bedroom-1.png',
 			name: 'Bedroom 1',
-			devices: [],
+			devices: [
+				{
+					id: 1,
+					icon: 'bulb',
+					name: 'lights',
+					state: 'on',
+				},
+				{
+					id: 2,
+					icon: 'socket',
+					name: 'socket',
+					state: 'off',
+				},
+			],
 		},
 		{
 			id: 'bedroom-2',
 			image: '/images/bedroom-2.png',
 			name: 'Bedroom 2',
-			devices: [],
+			devices: [
+				{
+					id: 1,
+					icon: 'bulb',
+					name: 'lights',
+					state: 'on',
+				},
+				{
+					id: 2,
+					icon: 'socket',
+					name: 'socket',
+					state: 'off',
+				},
+			],
 		},
 		{
 			id: 'others',
 			image: '/images/others-1.png',
 			name: 'Others',
-			devices: [],
+			devices: [
+				{
+					id: 1,
+					icon: 'bulb',
+					name: 'security lights',
+					state: 'on',
+				},
+				{
+					id: 2,
+					icon: 'water-drop',
+					name: 'pumping machine',
+					state: 'off',
+				},
+				{
+					id: 3,
+					icon: 'gate',
+					name: 'gate',
+					state: 'off',
+				},
+			],
 		},
 	],
 	error: null,
-	isLoading: true,
 };
 
 const roomSlice = createSlice({
 	name: 'rooms',
 	initialState,
 	reducers: {
-		addRooms(state, { payload }) {
-			const newRooms = state.data
-				? { ...state.data, ...payload.data }
-				: payload.data;
-			state.data = newRooms;
-			state.isLoading = false;
-		},
 		setRooms(state, { payload }) {
 			state.data = payload.data;
-			state.isLoading = false;
+		},
+		toggleDeviceState(state, { payload }) {
+			const room = state.data.find((room) => room.id === payload.roomId);
+			if (!room)
+				state.error = [
+					`Room with specified ID: ${payload.roomId} was not found.`,
+				];
+			else {
+				const device = room.devices.find((device) => device.id === payload.id);
+				if (!device)
+					state.error = [
+						`Device with specified ID: ${payload.device} in ${room.name} section was not found.`,
+					];
+				else {
+					device.state = payload.state;
+				}
+			}
 		},
 	},
 });
 
-export const { setRooms } = roomSlice.actions;
+export const { toggleDeviceState, setRooms } = roomSlice.actions;
 export default roomSlice.reducer;
