62aebfacc358560863e426f3c534d0e6dfaae0cc
kingkite <kitewebdevelopers@gmail.com>
2023-07-17 01:03:44 +0100

    Changed the checkauth to return children instead of Outlet

--- src/layout/protections/check-auth.jsx
+++ src/layout/protections/check-auth.jsx
@@ -1,37 +1,32 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import React from 'react';
+// import { useDispatch } from 'react-redux';
 
-import { auth } from '../../firebase';
-import { login, logout } from '../../store/features/auth';
+// import { auth } from '../../firebase';
+// import { login, logout } from '../../store/features/auth';
 
-function CheckAuth() {
-	const [loading, setLoading] = React.useState(true);
+function CheckAuth({ children }) {
+	// const dispatch = useDispatch();
 
-	const dispatch = useDispatch();
+	// React.useEffect(() => {
+	// 	onAuthStateChanged(auth, (user) => {
+	// 		if (user)
+	// 			dispatch(
+	// 				login({
+	// 					displayName: user.displayName,
+	// 					email: user.email,
+	// 					id: user.uid,
+	// 					image: user.photoURL,
+	// 					phone: user.phoneNumber,
+	// 				})
+	// 			);
+	// 		else dispatch(logout());
 
-	React.useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-			if (user)
-				dispatch(
-					login({
-						displayName: user.displayName,
-						email: user.email,
-						id: user.uid,
-						image: user.photoURL,
-						phone: user.phoneNumber,
-					})
-				);
-			else dispatch(logout());
+	// 		setLoading(false);
+	// 	});
+	// }, [dispatch]);
 
-			setLoading(false);
-		});
-	}, [dispatch]);
-
-	if (loading) return <div>Loading...</div>;
-
-	return <Outlet />;
+	return children;
 }
 
 export default CheckAuth;
