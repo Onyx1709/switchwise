078c219ba81d153b877caf772e511f8dd7b5bbdf
kingkite <kitewebdevelopers@gmail.com>
2023-07-19 11:17:28 +0100

    NAvigate to home page on logout click

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import routes from '../config/routes';
 import { logout } from '../firebase/auth';
 
 function useLogout() {
 	const [loading, setLoading] = React.useState(false);
+
+	const navigate = useNavigate();
 
 	const handleLogout = React.useCallback(async () => {
 		try {
 			setLoading(true);
 			const { error } = await logout();
 			if (error) window.alert(error.message);
+			else navigate(routes.HOME_PAGE);
 		} catch (error) {
 			window.alert(error.message);
 		} finally {
 			setLoading(false);
 		}
-	}, []);
+	}, [navigate]);
 
 	return {
 		loading,
