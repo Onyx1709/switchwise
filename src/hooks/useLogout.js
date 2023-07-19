857d6fc2280b1e815655da4550ca4aeee4c9c6c3
kingkite <kitewebdevelopers@gmail.com>
2023-07-19 12:12:36 +0100

    Using notification to display alerts

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -3,24 +3,34 @@ import { useNavigate } from 'react-router-dom';
 
 import routes from '../config/routes';
 import { logout } from '../firebase/auth';
+import { useNotificationContext } from '../store/contexts';
 
 function useLogout() {
 	const [loading, setLoading] = React.useState(false);
 
 	const navigate = useNavigate();
 
+	const { api } = useNotificationContext();
+
 	const handleLogout = React.useCallback(async () => {
 		try {
 			setLoading(true);
 			const { error } = await logout();
-			if (error) window.alert(error.message);
+			if (error)
+				api.error({
+					message: 'Unable to sign out!',
+					description: error.message,
+				});
 			else navigate(routes.HOME_PAGE);
 		} catch (error) {
-			window.alert(error.message);
+			api.error({
+				message: 'Unable to sign out!',
+				description: error.message,
+			});
 		} finally {
 			setLoading(false);
 		}
-	}, [navigate]);
+	}, [api, navigate]);
 
 	return {
 		loading,
